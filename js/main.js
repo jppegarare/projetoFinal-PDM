683e2a1980ea5a3157c74ef6f2f22bb8e37281ff
jppegarare <jppegarare18@gmail.com>
2025-01-30 14:18:29 -0400

    Começando a adicionar funcionalidades de Geolocalização

--- js/main.js
+++ js/main.js
@@ -1,8 +1,51 @@
-window.onload = () => {
-    "use strict";
-    if("serviceWorker" in navigator){
-        navigator.serviceWorker.register("./sw.js");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async() =>{
+        try{
+            let reg;
+            reg = await navigator.serviceWorker.register('/sw.js', {type: "module"})
+            console.log('Service Worker Registrada! 👌', reg)
+        }catch (err) {
+            console,log('Registro de Service Worker falhou 🤣', err)
+        }
+    })
+}
+
+let posicaoInicial;
+const capturarLocalizacao = document.getElementById('localizacao')
+const latitude = document.getElementById('latitude')
+const longitude =document.getElementById('longiude')
+
+const sucesso = (posicao) => {
+    posicaoInicial = posicao;
+    latitude.innerHTML = posicaoInicial.coords.latitude
+    longitude.innerHTML = posicaoInicial.coords.longitude
+}
+
+const erro = (error) => {
+    let errorMessage;
+    switch(error.code){
+        case 0:
+            errorMessage = "Erro desconhecido"
+        break;
+        case 1:
+            errorMessage = "Permissão Negada"
+        break;
+        case 2:
+            errorMessage = "Captura de posição indisponível"
+        break;
+        case 3:
+            errorMessage = "Tempo de solicitação exedido"
+        break;
     }
-};
+    console.log("Ocorreu um erro: " + errorMessage)
+}
 
-
+capturarLocalizacao.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(sucesso, erro)
+})
+// window.onload = () => {
+//     "use strict";
+//     if("serviceWorker" in navigator){
+//         navigator.serviceWorker.register("./sw.js");
+//     }
+// };
--- sw.js
+++ sw.js
@@ -1,20 +1,74 @@
-let cacheName = "pwa_final";
-let filesToCache = ["/", "/index", 
-                "/css/style.css", "/js/main.js"];
+import {warmStrategyCache} from 'workbox-recipes';
+import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies'
+import {registerRoute, Route} from 'workbox-routing'
+import {CacheableResponsePlugin} from 'workbox-cacheable-response'
+import {ExpirationPlugin} from 'workbox-expiration'
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+//cache
+const pageCache = new CacheFirst({
+  cacheName : "pwa-final",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds : 30 * 24 * 60 * 60,
+    }),
+  ],
+})
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+//indica cache
+warmStrategyCache({
+  urls: ["/", "/index", "/css/style.css", "/js/main.js"],
+  strategy: pageCache
+})
+
+//registro rota
+registerRoute(
+  ({request}) => ['style', 'script', 'worker']
+    .includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+)
+
+offlineFallback({
+  pageFallback: '/offline',
+})
+
+const imageRoute = new Route(({request}) => {
+  return request.destination === 'image';
+}, new CacheFirst({
+  cacheName: 'images',
+  plugins: [
+    new ExpirationPlugin({
+      maxAgeSeconds: 60 * 60 * 24 * 30, 
     })
-  );
-});
+  ]
+}))
+registerRoute(imageRoute)
+// let cacheName = "pwa_final";
+// let filesToCache = ["/", "/index", 
+//                 "/css/style.css", "/js/main.js"];
+
+// self.addEventListener("install", (e) => {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then((response) => {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
 
